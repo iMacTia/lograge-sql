7eee38d82866a2117cb41809a4aa7e6ad22badf8
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2016-07-15 15:21:45 +0100

    refactored extension using Module#prepend

--- lib/lograge/sql/extension.rb
+++ lib/lograge/sql/extension.rb
@@ -1,27 +1,29 @@
 module Lograge
-  class RequestLogSubscriber
-    def sql(event)
-      ActiveRecord::LogSubscriber.runtime += event.duration
-      return if event.payload[:name] == 'SCHEMA'
-      Thread.current[:lograge_sql_queries] ||= []
-      Thread.current[:lograge_sql_queries] << ("#{event.payload[:name]} (#{event.duration.to_f.round(2)}) #{event.payload[:sql]}")
-    end
+  module Sql
+    module Extension
+      def sql(event)
+        ActiveRecord::LogSubscriber.runtime += event.duration
+        return if event.payload[:name] == 'SCHEMA'
+        Thread.current[:lograge_sql_queries] ||= []
+        Thread.current[:lograge_sql_queries] << ("#{event.payload[:name]} (#{event.duration.to_f.round(2)}) #{event.payload[:sql]}")
+      end
 
-    alias_method :old_extract_request, :extract_request
-    def extract_request(event, payload)
-      data = old_extract_request(event, payload)
-      data.merge!(extract_sql_queries)
-    end
+      def extract_request(event, payload)
+        super.merge!(extract_sql_queries)
+      end
 
-    def extract_sql_queries
-      sql_queries = Thread.current[:lograge_sql_queries]
-      return {} unless sql_queries
+      def extract_sql_queries
+        sql_queries = Thread.current[:lograge_sql_queries]
+        return {} unless sql_queries
 
-      Thread.current[:lograge_sql_queries] = nil
-      { sql_queries: %('#{sql_queries.join("\n")}') }
+        Thread.current[:lograge_sql_queries] = nil
+        { sql_queries: %('#{sql_queries.join("\n")}') }
+      end
     end
   end
 end
+
+Lograge::RequestLogSubscriber.prepend Lograge::Sql::Extension
 
 ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
   Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
