511b99058698e2398345ecb3a850305dda0bda12
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2016-07-19 16:11:23 +0100

    fixed an issue with the previous version use of Module#prepend version bump to 0.1.2

--- lib/lograge/sql/extension.rb
+++ lib/lograge/sql/extension.rb
@@ -1,13 +1,6 @@
 module Lograge
   module Sql
     module Extension
-      def sql(event)
-        ActiveRecord::LogSubscriber.runtime += event.duration
-        return if event.payload[:name] == 'SCHEMA'
-        Thread.current[:lograge_sql_queries] ||= []
-        Thread.current[:lograge_sql_queries] << ("#{event.payload[:name]} (#{event.duration.to_f.round(2)}) #{event.payload[:sql]}")
-      end
-
       def extract_request(event, payload)
         super.merge!(extract_sql_queries)
       end
@@ -23,10 +16,21 @@ module Lograge
   end
 end
 
+module Lograge
+  class ActiveRecordLogSubscriber < ActiveSupport::LogSubscriber
+    def sql(event)
+      ActiveRecord::LogSubscriber.runtime += event.duration
+      return if event.payload[:name] == 'SCHEMA'
+      Thread.current[:lograge_sql_queries] ||= []
+      Thread.current[:lograge_sql_queries] << ("#{event.payload[:name]} (#{event.duration.to_f.round(2)}) #{event.payload[:sql]}")
+    end
+  end
+end
+
 Lograge::RequestLogSubscriber.prepend Lograge::Sql::Extension
 
 ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
   Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
 end
 
-Lograge::RequestLogSubscriber.attach_to :active_record
+Lograge::ActiveRecordLogSubscriber.attach_to :active_record
