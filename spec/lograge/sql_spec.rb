ce62a3e4f2100419da3e04b3862e34e411471b5a
iMacTia <giuffrida.mattia@gmail.com>
2019-05-07 13:50:25 +0100

    Adds proper test for Lograge extension.

--- spec/lograge/sql_spec.rb
+++ spec/lograge/sql_spec.rb
@@ -3,7 +3,21 @@
 require 'spec_helper'
 
 describe Lograge::Sql do
+  let(:subscriber) do
+    if defined?(Lograge::RequestLogSubscriber)
+      Lograge::RequestLogSubscriber
+    else
+      Lograge::LogSubscribers::ActionController
+    end
+  end
+
   it 'has a version number' do
     expect(Lograge::Sql::VERSION).not_to be nil
+  end
+
+  it 'extends lograge' do
+    require 'lograge/sql/extension'
+
+    expect(subscriber.new).to respond_to(:extract_sql_queries)
   end
 end
