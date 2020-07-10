15426d00f51f567c47ea0c43957ec527f3684cc5
Mattia <iMacTia@users.noreply.github.com>
2020-07-10 09:57:25 +0100

    Fixes Rubocop offenses

--- lib/lograge/sql.rb
+++ lib/lograge/sql.rb
@@ -15,11 +15,12 @@ module Lograge
       def setup(config)
         Lograge::Sql.formatter     = config.formatter     || default_formatter
         Lograge::Sql.extract_event = config.extract_event || default_extract_event
+        
+        return if config.keep_default_active_record_log
+        
         # Disable existing ActiveRecord logging
-        unless config.keep_default_active_record_log
-          ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
-            Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
-          end
+        ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
+          Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
         end
       end
 
